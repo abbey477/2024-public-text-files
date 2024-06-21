32c2ad9d60c10fae44dd4de764fbc2d7880ba633
abiodun okusolubo <abbey477@gmail.com>
2024-06-20 21:57:43 -0500

    Add files via upload

---
+++ json2csharp-respose/Datum.java
@@ -0,0 +1,4 @@
+public class Datum{
+    public String message;
+    public boolean error;
+}
---
+++ json2csharp-respose/Root.java
@@ -0,0 +1,5 @@
+public class Root{
+    public boolean success;
+    public String message;
+    public ArrayList<Datum> data;
+}
